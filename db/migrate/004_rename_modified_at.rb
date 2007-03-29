e1d8fd6ab39e9199146780c82dbf58c60a7f7fc3
zippy <zippy@f945c0c2-b2a2-4481-81d4-5ff5a83ded3a>
2007-03-29 20:22:08 +0000

    deleted erroneous migration file.
    git-svn-id: http://openmoney.rubyforge.org/svn/rubyom/trunk@10 f945c0c2-b2a2-4481-81d4-5ff5a83ded3a

--- db/migrate/004_rename_modified_at.rb
+++
@@ -1,9 +0,0 @@
-class RenameModifiedAt < ActiveRecord::Migration
-  def self.up
-    rename_column :links, :modified_at, :updated_at
-  end
-
-  def self.down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
