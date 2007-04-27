bf5cd5f3aaa9e57962a380fcfa925b4572503380
zippy <zippy@f945c0c2-b2a2-4481-81d4-5ff5a83ded3a>
2007-04-27 01:11:50 +0000

    extended link_spec.rb to specify the cases where linking should raise errors as well as not raise them because of trying to link incorrect types
    git-svn-id: http://openmoney.rubyforge.org/svn/rubyom/trunk@27 f945c0c2-b2a2-4481-81d4-5ff5a83ded3a

--- spec/models/links_spec.rb
+++ spec/models/links_spec.rb
@@ -4,30 +4,46 @@ context "linking entities" do
   fixtures :entities
   
   setup do
-    @link_types = %W(named_id approves created_by managed_by flow_to flow_from)
+    @link_types = %W(named_in approves created_by managed_by flow_to flow_from)
   end
 
   specify "should only link from context with: named_id, approves, created_by, managed_by link" do
+    from_omrl = "ca"
     { "named_in"=>"root",
       "approves"=>"tx1",
       "created_by"=>"mwl",
       "managed_by"=>"zippy"
-      }.each { |link_type,to_entity| lambda {create_link("ca",to_entity,link_type)}.should_not raise_error}
+    }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should_not raise_error}
+    { "uses" => "bucks",
+      "flow_to"=>"tx1",
+      "flow_from"=>"tx1",
+    }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
   end
 
   specify "should only link from currency with: named_id, approves, created_by, managed_by, uses link" do
+    from_omrl = "bucks"
     { "named_in"=>"us",
       "approves"=>"tx1",
       "created_by"=>"mwl",
       "managed_by"=>"zippy",
       "uses"=>"account"
-      }.each { |link_type,to_entity| lambda {create_link("bucks",to_entity,link_type)}.should_not raise_error}
+      }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should_not raise_error}
+    { "flow_to"=>"tx1",
+      "flow_from"=>"tx1",
+    }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
   end
 
   specify "should only link from account with: flow_to, flow_from link" do
+    from_omrl = "mwl"
     { "flow_from"=>"tx1",
       "flow_to"=>"tx1"
-      }.each { |link_type,to_entity| lambda {create_link("mwl",to_entity,link_type)}.should_not raise_error}
+    }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should_not raise_error}
+    { "named_in"=>"us",
+      "approves"=>"tx1",
+      "created_by"=>"mwl",
+      "managed_by"=>"zippy",
+      "uses"=>"account"
+    }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
   end
 
   specify "should always fail if linking from flow" do
