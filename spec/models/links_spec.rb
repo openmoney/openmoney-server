a8e3e1d609455f4ea828f62e975f08ebfc21c92e
zippy <zippy@f945c0c2-b2a2-4481-81d4-5ff5a83ded3a>
2007-04-27 01:19:36 +0000

    extended link_spec.rb to specify the cases where linking should raise errors as well as not raise them because of trying to link incorrect types
    git-svn-id: http://openmoney.rubyforge.org/svn/rubyom/trunk@28 f945c0c2-b2a2-4481-81d4-5ff5a83ded3a

--- spec/models/links_spec.rb
+++ spec/models/links_spec.rb
@@ -19,6 +19,22 @@ context "linking entities" do
       "flow_from"=>"tx1",
     }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
   end
+  specify "linking context to the wrong type of entity should fail" do
+    from_omrl = "bucks"
+    { "named_in"=>"bucks",
+      "named_in"=>"tx1",
+      "named_in"=>"mwl",
+      "approves"=>"us",
+      "approves"=>"bucks",
+      "approves"=>"mwl",
+      "created_by"=>"us",
+      "created_by"=>"bucks",
+      "created_by"=>"tx1",
+      "managed_by"=>"us",
+      "managed_by"=>"bucks",
+      "managed_by"=>"tx1",
+    }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
+  end
 
   specify "should only link from currency with: named_id, approves, created_by, managed_by, uses link" do
     from_omrl = "bucks"
@@ -30,6 +46,25 @@ context "linking entities" do
       }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should_not raise_error}
     { "flow_to"=>"tx1",
       "flow_from"=>"tx1",
+    }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
+  end
+  specify "linking currency to the wrong type of entity should fail" do
+    from_omrl = "bucks"
+    { "named_in"=>"mwl",
+      "named_in"=>"bucks",
+      "named_in"=>"tx1",
+      "approves"=>"us",
+      "approves"=>"bucks",
+      "approves"=>"mwl",
+      "created_by"=>"us",
+      "created_by"=>"bucks",
+      "created_by"=>"tx1",
+      "managed_by"=>"us",
+      "managed_by"=>"bucks",
+      "managed_by"=>"tx1",
+      "uses"=>"us",
+      "uses"=>"bucks",
+      "uses"=>"tx1",
     }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
   end
 
@@ -46,10 +81,19 @@ context "linking entities" do
     }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
   end
 
+  specify "linking account to the wrong type of entity should fail" do
+    from_omrl = "mwl"
+    { "flow_from"=>"ca",
+      "flow_from"=>"bucks",
+      "flow_to"=>"ca"
+      "flow_to"=>"bucks"
+    }.each { |link_type,to_entity| lambda {create_link(from_omrl,to_entity,link_type)}.should raise_error}
+  end
+
   specify "should always fail if linking from flow" do
     @link_types.each { |link_type| lambda {create_link("tx1","ca",link_type)}.should raise_error  }
   end
-    
+      
 end
 
 def create_link(from_omrl,to_omrl,link_type)
